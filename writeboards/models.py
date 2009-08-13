ff3db28541d5343ce99d1e153aa7844ee7a12098
Thomas Schreiber <tom@rizumu.us>
2009-08-13 16:36:06 -0600

    a first stab at the model

--- writeboards/models.py
+++ writeboards/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from tagging.models import Tag
+from tagging.fields import TagField
 
-# Create your models here.
+class Writeboard(models.model):
+    writeboard_name = models.CharField(_('writeboard name'), max_length=100)
+    slug = models.SlugField(_('slug'), unique=True)
+    creator = models.ForeignKey(_('creator'), User, related_name=_("creator"))
+    create_date = models.DateTimeField(_("created"), default=datetime.now)
+    writeboard_id = models.IntegerField(_('writeboard id'),)
+    tags = TagField()
+    active = models.BooleanField(default=True)    
+
+    def __unicode__(self):
+        return self.writeboard_name
+
+    class Meta(object):
+        verbose_name = _('writeboard')
+        verbose_name_plural = _('writeboards')
+        ordering=['modified']
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('writeboard_detail', None, {'slug': self.slug})
