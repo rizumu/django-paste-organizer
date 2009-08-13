dd949fdcd9834400f0534c054b56cb4d825a2be2
Thomas Schreiber <tom@rizumu.us>
2009-08-13 16:07:51 -0600

    Add package_data to setup.py

--- setup.py
+++ setup.py
@@ -7,19 +7,26 @@ setup(
     version=version,
     description="Manage 123.writeboard.com hosted writeboards in one place. Allows easier team collaboration on multiple writeboardsself.",
     long_description=read('README'),
-    classifiers=[
-        'Framework :: Django',
-        'Programming Language :: Python',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-    ],
-    keywords='writeboards,django'
     author='Thomas Schreiber',
     author_email='tom@insatsu.us',
     url='http://github.com/rizumu/django-writeboards/',
-    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    keywords='writeboards,django'
     packages=find_packages(),
     include_package_data=True,
+    package_data = {
+        'friends': [
+            'templates/writeboards/*/*.html',
+            'templates/writeboards/*/*.txt',
+        ]
+    },
     zip_safe=False,
 )
