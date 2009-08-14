2d7c2773a92a5b6780086b381ea793268476f682
Thomas Schreiber <tom@rizumu.us>
2009-08-13 18:14:27 -0600

    simplify admin and remove prepopulated slug

--- writeboards/admin.py
+++ writeboards/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from models import Writeboard
 
-class WriteboardAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('writeboard_name',)}
-
-admin.site.register(Writeboard, WriteboardAdmin)
+admin.site.register(Writeboard,)
